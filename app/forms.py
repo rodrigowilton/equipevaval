7a245ad59759cbbb5c68861b938718d99dff5d52
unknown <you@example.com>
2024-07-20 17:45:12 -0300

    alteracao

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,17 @@
-# forms.py
-
 from django import forms
 from .models import AppBanco
-
 
 class AppBancoForm(forms.ModelForm):
     class Meta:
         model = AppBanco
-        fields = ['nome', 'celular', 'cep', 'endereco', 'complemento', 'bairro', 'localidade', 'uf', 'numero']
+        fields = ['nome', 'celular', 'bairro', 'concorda_whatsapp']
         widgets = {
             'cep': forms.TextInput(attrs={'id': 'id_cep'}),
-            'logradouro': forms.TextInput(attrs={'id': 'id_endereco', 'disabled': 'disabled'}),
-            'complemento': forms.TextInput(attrs={'id': 'id_complemento', 'disabled': 'disabled'}),
-            'bairro': forms.TextInput(attrs={'id': 'id_bairro', 'disabled': 'disabled'}),
-            'localidade': forms.TextInput(attrs={'id': 'id_localidade', 'disabled': 'disabled'}),
-            'uf': forms.TextInput(attrs={'id': 'id_uf', 'disabled': 'disabled'}),
-            'numero': forms.TextInput(attrs={'id': 'id_numero', 'disabled': 'disabled'}),
+            'endereco': forms.TextInput(attrs={'id': 'id_endereco'}),
+            'complemento': forms.TextInput(attrs={'id': 'id_complemento'}),
+            'bairro': forms.TextInput(attrs={'id': 'id_bairro'}),
+            'localidade': forms.TextInput(attrs={'id': 'id_localidade'}),
+            'uf': forms.TextInput(attrs={'id': 'id_uf'}),
+            'numero': forms.TextInput(attrs={'id': 'id_numero'}),
+            'concorda_whatsapp': forms.CheckboxInput(attrs={'id': 'id_concorda_whatsapp'})
         }
--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-
 class AppBanco(models.Model):
-	nome = models.CharField(max_length=50, null=False, blank=False)
-	celular = models.CharField(max_length=15, null=False, blank=False)
-	cep = models.CharField(max_length=9,null=False, blank=False)
-	endereco = models.CharField(max_length=50, null=False, blank=False)
-	complemento = models.CharField(max_length=50, null=True, blank=True)
-	bairro = models.CharField(max_length=50, null=False, blank=False)
-	localidade = models.CharField(max_length=50, null=False, blank=False)
-	uf = models.CharField(max_length=2, null=False, blank=False)
-	numero = models.CharField(max_length=10, null=False, blank=False)
-	datacadastro = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    nome = models.CharField(max_length=50, null=False, blank=False)
+    celular = models.CharField(max_length=15, null=False, blank=False)
+    cep = models.CharField(max_length=9, null=True, blank=True)
+    endereco = models.CharField(max_length=50, null=True, blank=True)
+    complemento = models.CharField(max_length=50, null=True, blank=True)
+    bairro = models.CharField(max_length=50, null=False, blank=False)
+    localidade = models.CharField(max_length=50, null=True, blank=True)
+    uf = models.CharField(max_length=2, null=True, blank=True)
+    numero = models.CharField(max_length=10, null=True, blank=True)
+    datacadastro = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    concorda_whatsapp = models.BooleanField(default=False)  # Novo campo adicionado
